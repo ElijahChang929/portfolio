be42dc446161269cffb3c3c930751f87adb87100
Guangxin Zhang <100147603+ElijahChang929@users.noreply.github.com>
2024-05-05 13:54:01 +0000

    change about my homepage

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,9 +31,9 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "TOEFL 🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
+      "I am working on that.",
     url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
   },
   {
